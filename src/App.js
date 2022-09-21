a8facb84a5c33a5184b236f488053bcc02cbe0d5
kensonjohnson <kenson.johnson@outlook.com>
2022-09-20 19:37:14 -0500

    Created Recipe and RecipeList Components
    RecipeList.js can map over an array of Recipe and return them to the
    app.
    
    Recipe.js renders our individual recipe cards.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import RecipeList from "./RecipeList";
+
 function App() {
-  return <div className="App"></div>;
+  return <RecipeList recipes={sampleRecipes} />;
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: "Salted Chicken",
+    servings: 5,
+    cookTime: "1:45",
+    instructions: "1. Step One\n 2. Step Two\n 3. Step Three",
+  },
+  {
+    id: 1,
+    name: "Salted Fish",
+    servings: 4,
+    cookTime: "1:45",
+    instructions: "1. Step One\n 2. Step Two\n 3. Step Three",
+  },
+];
 
 export default App;
